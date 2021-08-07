9714cd35c1d9b7454f59f94591bc0fb4195d465b
Eike Rabetian <tommyrabetian@gmail.com>
2021-08-07 14:57:37 +0430

    Core module - config file

---
+++ backend/app/app/core/config.py
@@ -0,0 +1,35 @@
+import secrets
+from typing import List, Optional, Union
+
+from pydantic import AnyHttpUrl, BaseSettings, HttpUrl, validator
+
+
+class Settings(BaseSettings):
+    API_V1_STR: str = "/api/v1"
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    SERVER_NAME: str
+    SERVER_HOST: AnyHttpUrl
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+
+    @validator("BACKEND_CORS_ORIGINS", pre=True)
+    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
+        if isinstance(v, str) and not v.startswith("["):
+            return [i.strip() for i in v.split(",")]
+        elif isinstance(v, (list, str)):
+            return v
+        raise ValueError(v)
+
+    PROJECT_NAME: str
+    SENTRY_DSN: Optional[HttpUrl] = None
+
+    @validator("SENTRY_DSN", pre=True)
+    def sentry_dsn_can_be_blank(cls, v: str) -> Optional[str]:
+        if len(v) == 0:
+            return None
+        return v
+
+    class Config:
+        case_sensitive = True
+
+
+settings = Settings()
