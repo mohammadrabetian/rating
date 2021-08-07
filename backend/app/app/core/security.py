ddba8babc2f7032d5415c8ec6c01e0582bcf4d57
Eike Rabetian <tommyrabetian@gmail.com>
2021-08-07 17:05:03 +0430

    Security backend - API key inside header

---
+++ backend/app/app/core/security.py
@@ -0,0 +1,16 @@
+
+
+from fastapi import Security
+from fastapi.exceptions import HTTPException
+from fastapi.security.api_key import APIKeyHeader
+
+from app.core.config import settings
+
+API_KEY_NAME = "Authorization-Header"
+api_key_header = APIKeyHeader(name=API_KEY_NAME)
+
+
+async def get_api_key(api_key_header: str = Security(api_key_header)):
+    if api_key_header == settings.API_KEY_SECRET:
+        return api_key_header
+    raise HTTPException(status_code=403, detail="WRONG API KEY - not authorized")
